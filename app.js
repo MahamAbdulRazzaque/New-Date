6b6b51fc5aea83e7f962ad9a28806ba037a227ac
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-11-25 17:03:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+//let date = new Date();
+// document.write(date.getDay(), "<br>"); // 1
+// document.write(date.getDate(), "<br>"); //25
+// document.write(date.getMonth(), "<br>"); // 10
+// document.write(date.getFullYear(), "<br>"); // 2024
+// document.write(date.getHours(), "<br>"); // 16
+// document.write(date.getMinutes(), "<br>"); // 22
+// document.write(date.getSeconds(), "<br>"); // 35
+// document.write(date.getMilliseconds(), "<br>"); // 468555827
+//document.write(date.getTime(), "<br>");
+
+// document.write(date.setFullYear(2024, 10, 25));
+
+// let day = new Date("19-May-2032");
+// document.write(date , "<br>");
+// document.write(day);
+
+// let date = new Date();
+// date.setFullYear (2007);
+// document.write(date);
+
+// let date = new Date();
+// date.setDate(7);
+// document.write(date);
